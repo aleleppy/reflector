5973c2137d558c4282b94c9a1557897f21ae465c
aleleppy <alessandro@pinaculodigital.com.br>
2026-02-09 12:09:49 -0300

    fix: console log

--- src/object.property.ts
+++ src/object.property.ts
@@ -13,9 +13,9 @@ export class ObjectProp {
     this.name = name;
     this.type = referenceObject.$ref.split("/").at(-1) ?? "";
 
-    if (name === "companyReply") {
-      console.log(isRequired);
-    }
+    // if (name === "companyReply") {
+    //   console.log(isRequired);
+    // }
 
     this.required = isRequired ?? true; // tem que ver isso daí
     this.isNullable = !!isNullable;
